11dc9b078da930a163e7d893848652341ac82285
KOMINIARZ Anaïs <kominiarz.anais@gmail.com>
2020-04-08 15:51:02 +0200

    "Séparation des fichiers de config"

--- src/main/java/dev/AppSpringXML.java
+++ src/main/java/dev/AppSpringXML.java
@@ -21,7 +21,7 @@ public class AppSpringXML {
 	 */
 	public static void main(String[] args) {
 		
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-config-memoire.xml");
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-config-fichier.xml");
 		
 		// récupération du bean Menu
 		Menu menu = context.getBean(Menu.class);
