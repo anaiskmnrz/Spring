1fb976e93a8a338767db95c100ce8fea90f63d06
KOMINIARZ Anaïs <kominiarz.anais@gmail.com>
2020-04-08 15:36:55 +0200

    "TP 1.1"

---
+++ src/main/java/dev/AppSpringXML.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package dev;
+
+import java.util.Scanner;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import dev.ihm.Menu;
+
+/** Représentation 
+ *
+ * @author KOMINIARZ Anaïs
+ *
+ */
+public class AppSpringXML {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-config-memoire.xml");
+		
+		// récupération du bean Menu
+		Menu menu = context.getBean(Menu.class);
+		
+		// démarrage de l'application
+		menu.afficher();
+		
+		//fermeture du Scanner
+		context.getBean(Scanner.class).close();
+		
+		//fermeture du contexte Spring
+		context.close();
+
+	}
+
+}
