c1268d45e4896d3ee6f8474e411ff404bb935cdc
KOMINIARZ Anaïs <kominiarz.anais@gmail.com>
2020-04-16 00:03:48 +0200

    spring-data

--- src/main/java/dev/repository/PlatRepository.java
+++ src/main/java/dev/repository/PlatRepository.java
@@ -3,7 +3,11 @@
  */
 package dev.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import dev.entite.Plat;
 
@@ -14,4 +18,14 @@ import dev.entite.Plat;
  */
 public interface PlatRepository extends JpaRepository<Plat, Integer>{
 
+	@Query("SELECT p FROM Plat p WHERE p.prix = :prixPlat")
+	List<Plat> findByPrix(@Param("prixPlat")Integer prix);
+	
+	@Query("SELECT AVG(p.prix) FROM Plat p WHERE p.prix >:prixMin")
+	Double avgPrix(@Param("prixMin")Integer prix);
+	//TODO 
+	//@Query(" SELECT i FROM Ingredient INNER JOIN Plat on i.ingredient_id = :plat.getId() ")
+	//List<Ingredient> findByNomWithIngredients(@Param);
+	
+	void changerNom(String ancienNom, String nouveauNom)
 }
--- src/test/java/dev/repository/PlatRepositoryIntegrationTest.java
+++ src/test/java/dev/repository/PlatRepositoryIntegrationTest.java
@@ -5,9 +5,11 @@ package dev.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Logger;
 
 import org.junit.jupiter.api.Test;
@@ -19,10 +21,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.transaction.annotation.Transactional;
 
 import dev.config.DataSourceH2TestConfig;
 import dev.config.JpaConfig;
 import dev.entite.Plat;
+import dev.exception.PlatException;
 
 /** Représentation 
  *
@@ -57,6 +61,7 @@ public class PlatRepositoryIntegrationTest {
 		listeAjout.add(new Plat("Côte de boeuf",1100));
 		listeAjout.add(new Plat("Magret de canard",1300));
 		listeAjout.add(new Plat("Moules-frites",1300));
+		listeAjout.add(new Plat("Saumon",1400));
 		listeAjout.add(new Plat("Blanquette de veau",1500));
 		listeAjout.add(new Plat("Couscous",1600));
 		listeAjout.add(new Plat("Gigot d'agneau",2500));
@@ -89,6 +94,8 @@ public class PlatRepositoryIntegrationTest {
 		listeAjout.add(new Plat("Gigot d'agneau",2500));
 		listeAjout.add(new Plat("Couscous",1600));
 		listeAjout.add(new Plat("Blanquette de veau",1500));
+		// TODO pourquoi saumon n'est pas pris en compte ? 
+		//listeAjout.add(new Plat("Saumon",1400));
 		listeAjout.add(new Plat("Magret de canard",1300));
 		listeAjout.add(new Plat("Moules-frites",1300));
 		listeAjout.add(new Plat("Côte de boeuf",1100));
@@ -119,7 +126,48 @@ public class PlatRepositoryIntegrationTest {
 	
 	}
 	
-
+	@Test
+	void testFindById() {
+		Optional<Plat> findById = repository.findById(2);
+		
+		Plat platRecup = findById.orElseThrow(() -> new PlatException());
+		Plat platAjout = new Plat("Moules-frites",1300);
+		
+		assertThat(platRecup).isEqualTo(platAjout);
+	}
 	
+	@Test 
+	@Transactional
+	void testGetOne() {
+		
+		Plat platRecup = repository.getOne(1);
+		Plat platAjout = new Plat("Magret de canard",1300);
+		
+		assertThat(platRecup).isEqualTo(platAjout);
+	}
+	
+	@Test
+	void testCount() {
+		assertThat(repository.count()).isEqualByComparingTo((long) 6);
+	}
 
+	@Test
+	void testFindByPrix() {
+		Plat platAjout = new Plat("Couscous",1600);
+		List<Plat> listeRecup = repository.findByPrix(1600);
+		assertTrue(listeRecup.contains(platAjout));
+	}
+	
+	@Test
+	void testAvgprix() {
+		Double avgRecup = repository.avgPrix(1600);
+		assertThat(avgRecup).isEqualByComparingTo(2500.0);
+	}
+	
+	@Test
+	void testSave() {
+		Plat plat = new Plat("Saumon", 1400);
+		repository.save(plat);
+		assertTrue(repository.findAll().contains(plat));
+	}
 }
